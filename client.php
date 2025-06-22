de98838b599fcbc748c5f06dc46b9314475168fc
khadija-111 <fsjes22khadija@gmail.com>
2025-06-22 17:04:30 +0100

    Update client.php

--- client.php
+++ client.php
@@ -66,7 +66,7 @@ $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $titre ?></title>
+    <title>clients</title>
 
      
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
